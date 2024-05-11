bc772b156d391d1672d28d540de6f873d16a5303
GNYAR <s901121s901121@gmail.com>
2024-05-12 00:42:19 +0800

    feat(scanplate): block for camera and upload button

--- src/components/ScanPlate.vue
+++ src/components/ScanPlate.vue
@@ -1,6 +1,14 @@
 <script setup></script>
 
 <template>
+  <div class="position-relative">
+    <div class="position-absolute w-100 h-100 d-flex justify-center align-end pb-4">
+      <v-btn rounded text="上傳照片"></v-btn>
+    </div>
+
+    <v-sheet width="100%" height="440px" color="primary"> 鏡頭畫面 </v-sheet>
+  </div>
+
   <div>www</div>
 </template>
 
