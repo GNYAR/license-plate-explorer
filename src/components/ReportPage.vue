2cc29bb4c558309f8967a4c0d4903025f251be48
GNYAR <s901121s901121@gmail.com>
2024-05-23 00:43:42 +0800

    feat(report): station list

--- src/components/ReportPage.vue
+++ src/components/ReportPage.vue
@@ -4,12 +4,34 @@ import QueryResult from '@/components/QueryResult.vue'
 
 <template>
   <div class="mx-4">
-    <v-btn class="float-right" color="primary" text="查看照片" rounded></v-btn>
+    <v-btn class="float-right mt-4" color="primary" text="查看照片" rounded></v-btn>
 
-    <QueryResult class="text-start mb-10" v-bind="result"></QueryResult>
+    <QueryResult class="text-start mb-4" v-bind="result"></QueryResult>
 
-    <v-text-field density="compact" label="在哪裡發現?" variant="solo" hide-details></v-text-field>
+    <v-text-field
+      density="compact"
+      label="在哪裡發現?"
+      variant="underlined"
+      hide-details
+    ></v-text-field>
+    <v-sheet class="my-2" width="100%" height="250px" color="primary"> 地圖 </v-sheet>
   </div>
+
+  <div class="px-4 mt-2">附近警察機關</div>
+  <v-list>
+    <v-list-item v-for="x in stations" :key="x.name">
+      <v-list-item-title>
+        <div class="d-flex">
+          <v-icon class="my-auto mr-2" color="warning">mdi-police-badge</v-icon>
+          <div class="text-h6 font-weight-bold">{{ x.name }}</div>
+          <div class="ml-auto text-grey">{{ x.dist }} m</div>
+        </div>
+      </v-list-item-title>
+      地址：<a href="">{{ x.address }}</a> <br />
+      電話：<a :href="'tel:' + x.tel">{{ x.tel }}</a>
+      <v-divider class="my-2"></v-divider>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
@@ -20,7 +42,33 @@ export default {
         license: '505-LRP',
         isFound: true,
         time: '113/05/05 01:36:03'
-      }
+      },
+      stations: [
+        {
+          name: '正濱派出所',
+          address: '202經龍是中正區中正路786之1號',
+          tel: '02 2462 1889',
+          dist: 100
+        },
+        {
+          name: '正濱派出所',
+          address: '202經龍是中正區中正路786之1號',
+          tel: '02 2462 1889',
+          dist: 100
+        },
+        {
+          name: '正濱派出所',
+          address: '202經龍是中正區中正路786之1號',
+          tel: '02 2462 1889',
+          dist: 100
+        },
+        {
+          name: '正濱派出所',
+          address: '202經龍是中正區中正路786之1號',
+          tel: '02 2462 1889',
+          dist: 100
+        }
+      ]
     }
   }
 }
