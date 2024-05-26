497e98729be61cdacd29f91c454b68127c7d07fa
GNYAR <s901121s901121@gmail.com>
2024-05-26 23:50:18 +0800

    feat(query): query from input box

--- src/components/InputTab.vue
+++ src/components/InputTab.vue
@@ -5,6 +5,7 @@ import QueryResult from '@/components/QueryResult.vue'
 <template>
   <div class="my-10">
     <v-text-field
+      v-model="input"
       :loading="loading"
       append-inner-icon="mdi-magnify"
       class="mx-auto"
@@ -14,37 +15,71 @@ import QueryResult from '@/components/QueryResult.vue'
       variant="outlined"
       hide-details
       width="300"
-      @click:append-inner="onClick"
-      @keydown.enter.prevent="onClick"
+      @click:append-inner="checkVehicle(input)"
+      @keydown.enter.prevent="checkVehicle(input)"
       autofocus
     ></v-text-field>
   </div>
 
-  <QueryResult v-if="loaded" v-bind="result"></QueryResult>
+  <QueryResult v-if="result" v-bind="result"></QueryResult>
 </template>
 
 <script>
+import { entries, filter, first, isEmpty, join, map, pipe, prop, propEq } from 'lodash/fp'
+
 export default {
   data() {
     return {
-      result: {
-        license: '505-LRP',
-        isFound: true,
-        time: '113/05/05 01:36:03'
-      },
+      result: null,
+      input: null,
       loaded: false,
       loading: false
     }
   },
 
   methods: {
-    onClick() {
+    async checkVehicle(id) {
       this.loading = true
 
-      setTimeout(() => {
-        this.loading = false
-        this.loaded = true
-      }, 2000)
+      const params = {
+        // CarType: A(汽車)、B(重機車)、C(輕機車)、G(動力機械車)
+        ajaxAction: 'ClQuery',
+        QS_CarType1: 'A',
+        QS_CarId1: id,
+        QS_CarType2: 'B',
+        QS_CarId2: id,
+        QS_CarType3: 'C',
+        QS_CarId3: id,
+        QS_CarType4: 'G',
+        QS_CarId4: id,
+        QS_CarType5: null,
+        QS_CarId5: null,
+        FUNC_NAME: 'CL'
+      }
+      const URL = 'https://eze8.npa.gov.tw/NpaE8ServerRWD/CaseQueryServlet'
+      const headers = { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }
+      const toForm = pipe([entries, map(join('=')), join('&')])
+      const body = toForm(params)
+
+      const decode = pipe([
+        prop('formData'),
+        (html) => {
+          const div = document.createElement('div')
+          div.innerHTML = html
+          return div.innerText
+        },
+        JSON.parse
+      ])
+
+      const res = await fetch(URL, { method: 'post', headers, body })
+        .then((x) => x.json())
+        .then(decode)
+        .then(filter(propEq('E8_STATUS', '車輛失竊')))
+
+      this.result = isEmpty(res)
+        ? { id, isStolen: false }
+        : { id, isStolen: true, type: pipe([first, prop('E8_CAR_TYPE_NM')])(res) }
+      this.loading = false
     }
   }
 }
