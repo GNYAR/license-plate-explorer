df95ff0af952034776ead87effa2c52da813bc7c
GNYAR <s901121s901121@gmail.com>
2024-05-30 20:58:20 +0800

    build(base): use environment variable to set base path

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const base = process.env.NODE_ENV === 'production' ? '/license-plate-explorer/' : '/'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base,
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    base: process.env?.VITE_BASE ?? '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  })
+}
