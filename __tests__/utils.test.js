9fe116ebdd5e278855f9aac234c6efbd488a5af4
GNYAR <s901121s901121@gmail.com>
2024-06-02 02:03:56 +0800

    test(utils): test for queryStolenAPI

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,7 +1,25 @@
 import { test, describe, expect } from 'vitest'
+import { queryStolenAPI } from '@/utils'
 
-describe('測試', () => {
-  test('1 + 1 = 2', () => {
-    expect(1 + 1).toBe(2)
+describe('Query Stolen API', () => {
+  test('safe scooter', async () => {
+    const { id, isStolen, type } = await queryStolenAPI('505-LRP')
+    expect(id).toBe('505-LRP')
+    expect(isStolen).toBeFalsy()
+    expect(type).toBeUndefined()
+  })
+
+  test('stolen scooter', async () => {
+    const { id, isStolen, type } = await queryStolenAPI('JFC-961')
+    expect(id).toBe('JFC-961')
+    expect(isStolen).toBeTruthy()
+    expect(type).toBe('重機車')
+  })
+
+  test('stolen car', async () => {
+    const { id, isStolen, type } = await queryStolenAPI('1958-ZE')
+    expect(id).toBe('1958-ZE')
+    expect(isStolen).toBeTruthy()
+    expect(type).toBe('汽車')
   })
 })
